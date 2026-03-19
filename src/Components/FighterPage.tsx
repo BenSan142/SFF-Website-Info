1f68821f763eda01fac91d9ed970f45cf9bffb96
Benito <benitosan14@gmail.com>
2026-03-19 15:34:44 -0700

    Add on to setup for Fighter Pages

--- src/Components/FighterPage.tsx
+++ src/Components/FighterPage.tsx
@@ -1,19 +1,39 @@
 import Header from "../Components/Header";
 import '../App.css';
 
-
-function FighterPage({ name, image, alt, children } : { name: string; image: string; alt: string; children: React.ReactNode }) {
+function FighterPage({
+  name,
+  image,
+  alt,
+  children
+}: {
+  name: string;
+  image: string;
+  alt: string;
+  children: React.ReactNode;
+}) {
   return (
     <div className="fighter-layout">
       <Header />
 
-      <h1 className="FighterText">{name}</h1>
+      <div className="fighter-section">
 
+        <img
+          src={image}
+          alt={alt}
+          className="fighter-image-2"
+        />
 
-      <img src={image} alt={alt} className="fighter-image-2" />
+        <div className="fighter-info">
 
-      <div className="fighter-content">
-        {children}
+          <h1>{name}</h1>
+
+          <div className="fighter-text">
+            {children}
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
--- src/Pages/BenSan14.tsx
+++ src/Pages/BenSan14.tsx
@@ -8,8 +8,14 @@ function BenSan14Page() {
       image={FighterImg}
       alt="BenSan14"
     >
-      <p className="FighterText">
+      <p>
         BenSan14 is a skilled fighter in the SFF.
+      </p>
+      <p>
+        He has a strong background in martial arts and is known for his agility and precision in combat.
+      </p>
+      <p>
+        BenSan14 has participated in several tournaments and has a reputation for being a formidable opponent.
       </p>
     </FighterPage>
   );
