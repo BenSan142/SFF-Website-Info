7c00431d739a24ea649b50bf9b47708d83a0fe6a
Benito <benitosan14@gmail.com>
2026-03-25 20:03:56 -0700

    Update Fighter Data/More Fighter Bios

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import './App.css';
 import { HashRouter as Router, Route} from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import BenSan14Page from './Pages/BenSan14';
-import TheMightySinghPage from './Pages/TheMightySingh';
-import HendrixPage from './Pages/Hendrix';
+//import BenSan14Page from './Pages/BenSan14';
+//import TheMightySinghPage from './Pages/TheMightySingh';
+//import HendrixPage from './Pages/Hendrix';
+import FighterPageDynamic from "./Pages/FighterPageDynamic";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="fighter/BenSan14" element={<BenSan14Page />} />
-        <Route path="fighter/TheMightySingh" element={<TheMightySinghPage />} />
-        <Route path="fighter/Hendrix" element={<HendrixPage />} />
+        <Route path="/fighter/:id" element={<FighterPageDynamic />} />
+        {/* <Route path="/fighter/BenSan14" element={<BenSan14Page />} />
+        <Route path="/fighter/TheMightySingh" element={<TheMightySinghPage />} />
+        <Route path="/fighter/Hendrix" element={<HendrixPage />} /> */}
+        {/* Add more routes for other fighters as needed */}
       </Routes>
     </Router>
   );
